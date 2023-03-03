eea32e0144304083fcb356e76fb4e332ba056cd5
yasin.yildirim <yasin.yildirim>
2023-03-03 10:03:07 +0300

    add

---
+++ src/main/java/com/yil/document/model/Document.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023. Tüm hakları Yasin Yıldırım'a aittir.
+ */
+
+package com.yil.document.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(schema = "DOC", name = "DOCUMENT")
+public class Document {
+
+    @Id
+    @Column(name = "ID", updatable = false)
+    private Long id;
+    @Column(name = "HASH_VALUE", updatable = false)
+    private String hashValue;
+    @Column(name = "CONTENT", updatable = false)
+    private byte[] content;
+    @Column(name = "LENGTH", updatable = false)
+    private Integer length;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_DATE", updatable = false)
+    private Date createdDate;
+    @Column(name = "CREATED_USER_ID", updatable = false)
+    private Long createdUserId;
+
+}
